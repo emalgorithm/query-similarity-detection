b89117fb1c4c1199878c3240ba623875e59e174f
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-04-27 22:49:12 -0400

    Add location levels constant

--- constants.py
+++ constants.py
@@ -14,12 +14,16 @@ class AlgorithmName(Enum):
 
 
 class Constants(object):
+    LOCATION_LEVELS = [Resolution.LOCATION_LEVEL_1,
+                       Resolution.LOCATION_LEVEL_2,
+                       Resolution.LOCATION_LEVEL_2]
+
     LEVEL_1_LOCATIONS = ["Imola", "Faenza", "Castel Bolognese", "Lugo"]
     LEVEL_2_LOCATIONS = ["Bologna", "Ferrara", "Cesena", "Modena"]
     LEVEL_3_LOCATIONS = ["Emilia-Romagna", "Toscana", "Abruzzo"]
 
     MIN_DATE = "01/01/2016"
-    MIN_TIMESTAMP = int(datetime.datetime.strptime(MIN_DATE, "%d/%m/%Y").timestamp())
+    MIN_TIMESTAMP = int(datetime.strptime(MIN_DATE, "%d/%m/%Y").timestamp())
     MAX_DATE = "31/12/2017"
-    MAX_TIMESTAMP = int(datetime.datetime.strptime(MAX_DATE, "%d/%m/%Y").timestamp())
+    MAX_TIMESTAMP = int(datetime.strptime(MAX_DATE, "%d/%m/%Y").timestamp())
 
