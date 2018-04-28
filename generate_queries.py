981924e56f08b4927be7bcf61bcd412ee6f29e95
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-04-28 00:11:16 -0400

    Implement code to implement similar and not similar queries

--- generate_queries.py
+++ generate_queries.py
@@ -4,19 +4,68 @@ import random
 from constants import Resolution, AlgorithmName, Constants
 
 
-def generate_similar_density_queries_pair():
+def generate_density_queries_pair(similar=False):
     """
-    Generates two density queries which are similar to each other.
-    Two density queries are similar when:
-    - resolution = resolution’
-    - keySelector = keySelector’
-    - |startDate - startDate’| < 60 minutes
-    - |endDate - endDate’| < 60 minutes
+    Generates two density queries which are not similar to each other.
     """
     q1 = generate_random_density_query()
-    q2 = generate_similar_density_query(q1)
+    if similar:
+        q2 = generate_similar_density_query(q1)
+    else:
+        q2 = generate_not_similar_density_query(q1)
 
     return q1, q2
+
+
+def generate_mobility_queries_pair(similar=False):
+    """
+    Generates two density queries which are not similar to each other.
+    """
+    q1 = generate_random_mobility_query()
+    if similar:
+        q2 = generate_similar_mobility_query(q1)
+    else:
+        q2 = generate_not_similar_mobility_query(q1)
+
+    return q1, q2
+
+
+def generate_not_similar_density_query(q1):
+    q2 = generate_random_density_query()
+
+    while are_density_queries_similar(q1, q2):
+        q2 = generate_random_density_query()
+
+    return q2
+
+
+def generate_not_similar_mobility_query(q1):
+    q2 = generate_random_density_query()
+
+    while are_mobility_queries_similar(q1, q2):
+        q2 = generate_random_density_query()
+
+    return q2
+
+
+def are_density_queries_similar(q1, q2):
+    return are_timestamps_similar(q1.start_date, q2.start_date) and \
+           are_timestamps_similar(q1.end_date, q2.end_date) and \
+           q1.resolution == q2.resolution and \
+           q1.key_selector == q2.key_selector
+
+
+def are_mobility_queries_similar(q1, q2):
+    return are_timestamps_similar(q1.start_date, q2.start_date) and \
+           are_timestamps_similar(q1.end_date, q2.end_date) and \
+           are_timestamps_similar(q1.params["first_window"], q2.params["first_window"]) and \
+           are_timestamps_similar(q1.params["second_window"], q2.params["second_window"]) and \
+           q1.resolution == q2.resolution and \
+           q1.key_selector == q2.key_selector
+
+
+def are_timestamps_similar(t1, t2):
+    return abs(t1 - t2) < 60 * 60
 
 
 def generate_random_density_query():
@@ -34,7 +83,7 @@ def generate_random_density_query():
 
 def generate_similar_density_query(query):
     """
-        Generates a query similar to the given one.
+        Generates a density query similar to the given density one.
         Two density queries are similar when:
         - resolution = resolution’
         - keySelector = keySelector’
@@ -87,8 +136,8 @@ def generate_random_mobility_query():
 
 def generate_similar_mobility_query(query):
     """
-        Generates a query similar to the given one.
-        Two density queries are similar when:
+        Generates a mobility query similar to the given mobility query.
+        Two mobility queries are similar when:
         - resolution = resolution’
         - keySelector = keySelector’
         - |startDate - startDate’| < 60 minutes
@@ -111,6 +160,33 @@ def generate_similar_mobility_query(query):
     return Query(algorithm_name=query.algorithm_name, start_date=similar_start_date,
                  end_date=similar_end_date, params=params, resolution=query.resolution,
                  key_selector=query.key_selector, sample=different_sample)
+
+
+# def generate_similar_mobility_query_to_density_query(query):
+#     """
+#         Generates a mobility query similar to the given density query.
+#         Two mobility queries are similar when:
+#         - resolution = resolution’
+#         - keySelector = keySelector’
+#         # TODO
+#     """
+#     similar_start_date = generate_similar_timestamp(query.start_date)
+#     similar_end_date = generate_similar_timestamp(query.end_date, min_timestamp=similar_start_date)
+#     similar_first_window = generate_similar_timestamp(query.params["first_window"],
+#                                                       min_timestamp=similar_start_date,
+#                                                       max_timestamp=similar_end_date)
+#     similar_second_window = generate_similar_timestamp(query.params["second_window"],
+#                                                        min_timestamp=similar_first_window,
+#                                                        max_timestamp=similar_end_date)
+#     params = {
+#         "first_window": similar_first_window,
+#         "second_window": similar_second_window
+#     }
+#     different_sample = random.uniform(0, 1)
+#
+#     return Query(algorithm_name=query.algorithm_name, start_date=similar_start_date,
+#                  end_date=similar_end_date, params=params, resolution=query.resolution,
+#                  key_selector=query.key_selector, sample=different_sample)
 
 
 def generate_random_timestamp(min_timestamp=Constants.MIN_TIMESTAMP, max_timestamp=Constants.MAX_TIMESTAMP):
