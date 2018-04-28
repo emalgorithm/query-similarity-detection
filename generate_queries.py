a94b61c6dedc8a1604be6f42d0177b5dd444d584
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-04-27 23:40:13 -0400

    Generate mobility queries

--- generate_queries.py
+++ generate_queries.py
@@ -42,11 +42,74 @@ def generate_similar_density_query(query):
         - |endDate - endDate’| < 60 minutes
     """
     similar_start_date = generate_similar_timestamp(query.start_date)
-    similar_end_date = generate_similar_timestamp(query.end_date)
+    similar_end_date = generate_similar_timestamp(query.end_date, min_timestamp=similar_start_date)
     different_sample = random.uniform(0, 1)
 
     return Query(algorithm_name=query.algorithm_name, start_date=similar_start_date,
                  end_date=similar_end_date, params=query.params, resolution=query.resolution,
+                 key_selector=query.key_selector, sample=different_sample)
+
+
+def generate_similar_mobility_queries_pair():
+    """
+        Generates two mobility queries which are similar to each other.
+        Two density queries are similar when:
+        - resolution = resolution’
+        - keySelector = keySelector’
+        - |startDate - startDate’| < 60 minutes
+        - |first_window - first_window’| < 60 minutes
+        - |endDate - endDate’| < 60 minutes
+        - |second_window - second_window’| < 60 minutes
+    """
+    q1 = generate_random_mobility_query()
+    q2 = generate_similar_mobility_query(q1)
+
+    return q1, q2
+
+
+def generate_random_mobility_query():
+    algorithm_name = AlgorithmName.MOBILITY
+    start_date = generate_random_timestamp()
+    end_date = generate_random_timestamp(min_timestamp=start_date)
+    first_window = generate_random_timestamp(min_timestamp=start_date, max_timestamp=end_date)
+    second_window = generate_random_timestamp(min_timestamp=first_window, max_timestamp=end_date)
+    params = {
+        "first_window": first_window,
+        "second_window": second_window
+              }
+    resolution = random.choice(Constants.LOCATION_LEVELS)
+    key_selector = generate_random_location(resolution) + "." + generate_random_location(resolution)
+    sample = random.uniform(0, 1)
+
+    return Query(algorithm_name=algorithm_name, start_date=start_date, end_date=end_date,
+                 params=params, resolution=resolution, key_selector=key_selector, sample=sample)
+
+
+def generate_similar_mobility_query(query):
+    """
+        Generates a query similar to the given one.
+        Two density queries are similar when:
+        - resolution = resolution’
+        - keySelector = keySelector’
+        - |startDate - startDate’| < 60 minutes
+        - |endDate - endDate’| < 60 minutes
+    """
+    similar_start_date = generate_similar_timestamp(query.start_date)
+    similar_end_date = generate_similar_timestamp(query.end_date, min_timestamp=similar_start_date)
+    similar_first_window = generate_similar_timestamp(query.params["first_window"],
+                                                      min_timestamp=similar_start_date,
+                                                      max_timestamp=similar_end_date)
+    similar_second_window = generate_similar_timestamp(query.params["second_window"],
+                                                       min_timestamp=similar_first_window,
+                                                       max_timestamp=similar_end_date)
+    params = {
+        "first_window": similar_first_window,
+        "second_window": similar_second_window
+              }
+    different_sample = random.uniform(0, 1)
+
+    return Query(algorithm_name=query.algorithm_name, start_date=similar_start_date,
+                 end_date=similar_end_date, params=params, resolution=query.resolution,
                  key_selector=query.key_selector, sample=different_sample)
 
 
@@ -80,28 +143,3 @@ def generate_random_location(resolution):
     elif resolution == Resolution.LOCATION_LEVEL_2:
         return random.choice(Constants.LEVEL_2_LOCATIONS)
     return random.choice(Constants.LEVEL_3_LOCATIONS)
-
-# def generate_random_query():
-#     startDate = generate_random_datetime()
-#     endDate = generate_random_datetime(min_year=startDate.year)
-#     algorithm = np.random.choice(['density', 'migration', 'commuting'])
-#     params = {}
-#     aggregationLevel = np.random.choice(['region', 'commune', 'antenna'])
-#     aggregationValue = np.random.choice(['Dakar', 'London', 'Rome'])
-#     sample = 1
-#
-#     query = Query(startDate, endDate, algorithm, params, aggregationLevel, aggregationValue, sample)
-#     return query
-#
-# def generate_random_queries(n=100):
-# 	return [generate_random_query() for i in range(n)]
-#
-# def generate_random_datetime(min_year=1900, max_year=datetime.now().year):
-#     start = datetime(min_year, 1, 1, 00, 00, 00)
-#     years = max_year - min_year + 1
-#     end = start + timedelta(days=365 * years)
-#
-#     return start + (end - start) * random.random()
-#
-#
-# print(generate_random_queries())
