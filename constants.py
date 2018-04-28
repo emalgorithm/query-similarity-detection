c49cd1a1f3c3a6e1f645fc978b2e1c89adc6d192
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-04-27 22:20:39 -0400

    Add constants for project

---
+++ constants.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from datetime import datetime
+
+
+class Resolution(Enum):
+    LOCATION_LEVEL_1 = "location_level_1"
+    LOCATION_LEVEL_2 = "location_level_2"
+    LOCATION_LEVEL_3 = "location_level_3"
+
+
+class AlgorithmName(Enum):
+    DENSITY = "density"
+    MOBILITY = "mobility"
+
+
+class Constants(object):
+    LEVEL_1_LOCATIONS = ["Imola", "Faenza", "Castel Bolognese", "Lugo"]
+    LEVEL_2_LOCATIONS = ["Bologna", "Ferrara", "Cesena", "Modena"]
+    LEVEL_3_LOCATIONS = ["Emilia-Romagna", "Toscana", "Abruzzo"]
+
+    MIN_DATE = "01/01/2016"
+    MIN_TIMESTAMP = int(datetime.datetime.strptime(MIN_DATE, "%d/%m/%Y").timestamp())
+    MAX_DATE = "31/12/2017"
+    MAX_TIMESTAMP = int(datetime.datetime.strptime(MAX_DATE, "%d/%m/%Y").timestamp())
+
