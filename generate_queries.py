6075e5d4f192765c4205924915bf25f3a731a51b
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-04-27 23:04:51 -0400

    Refactor code into more functions

--- generate_queries.py
+++ generate_queries.py
@@ -4,7 +4,7 @@ import random
 from constants import Resolution, AlgorithmName, Constants
 
 
-def generate_similar_density_queries():
+def generate_similar_density_queries_pair():
     """
     Generates two density queries which are similar to each other.
     Two density queries are similar when:
@@ -13,6 +13,13 @@ def generate_similar_density_queries():
     - |startDate - startDate’| < 60 minutes
     - |endDate - endDate’| < 60 minutes
     """
+    q1 = generate_random_density_query()
+    q2 = generate_similar_density_query(q1)
+
+    return q1, q2
+
+
+def generate_random_density_query():
     algorithm_name = AlgorithmName.DENSITY
     start_date = generate_random_timestamp()
     end_date = generate_random_timestamp(min_timestamp=start_date)
@@ -20,17 +27,27 @@ def generate_similar_density_queries():
     resolution = random.choice(Constants.LOCATION_LEVELS)
     key_selector = generate_random_location(resolution)
     sample = random.uniform(0, 1)
-    q1 = Query(algorithm_name=algorithm_name, start_date=start_date, end_date=end_date,
-               params=params, resolution=resolution, key_selector=key_selector, sample=sample)
 
-    similar_start_date = generate_similar_timestamp(start_date)
-    similar_end_date = generate_similar_timestamp(end_date)
+    return Query(algorithm_name=algorithm_name, start_date=start_date, end_date=end_date,
+                 params=params, resolution=resolution, key_selector=key_selector, sample=sample)
+
+
+def generate_similar_density_query(query):
+    """
+        Generates a query similar to the given one.
+        Two density queries are similar when:
+        - resolution = resolution’
+        - keySelector = keySelector’
+        - |startDate - startDate’| < 60 minutes
+        - |endDate - endDate’| < 60 minutes
+    """
+    similar_start_date = generate_similar_timestamp(query.start_date)
+    similar_end_date = generate_similar_timestamp(query.end_date)
     different_sample = random.uniform(0, 1)
-    q2 = Query(algorithm_name=algorithm_name, start_date=similar_start_date,
-               end_date=similar_end_date, params=params, resolution=resolution,
-               key_selector=key_selector, sample=different_sample)
 
-    return q1, q2
+    return Query(algorithm_name=query.algorithm_name, start_date=similar_start_date,
+                 end_date=similar_end_date, params=query.params, resolution=query.resolution,
+                 key_selector=query.key_selector, sample=different_sample)
 
 
 def generate_random_timestamp(min_timestamp=Constants.MIN_TIMESTAMP, max_timestamp=Constants.MAX_TIMESTAMP):
