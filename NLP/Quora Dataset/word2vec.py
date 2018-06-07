650e6052660613b08d84a01c6b33b2c8be6b8b1a
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-06-07 15:27:44 +0000

    Update API of word2vec similarity model

--- NLP/Quora Dataset/word2vec.py
+++ NLP/Quora Dataset/word2vec.py
@@ -3,9 +3,12 @@ import gensim
 class Word2Vec():
     def __init__(self):
         self.model = gensim.models.KeyedVectors.load_word2vec_format('./data/GoogleNews-vectors-negative300.bin', binary=True)
+    
+    def contains_word(self, word):
+        return word in self.model.vocab
         
-    def get_closest_word(self, word):
+    def most_similar(self, word):
         return self.model.most_similar([word], topn=1)[0][0]
     
-    def get_closest_words(self, word, n):
+    def most_similar_words(self, word, n):
         return self.model.most_similar([word], topn=n)
