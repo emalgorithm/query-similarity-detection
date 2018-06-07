f4c24dab3cd0a4b831aeb045dec6c25e606f5580
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-06-07 15:51:29 +0000

    Improve logging in adversarial algo

--- NLP/Quora Dataset/adversarial_algos.py
+++ NLP/Quora Dataset/adversarial_algos.py
@@ -1,6 +1,6 @@
-def adversarial_white_box_change(q1, q2, model, tp, word_similarity, threshold=0.4):
+def adversarial_white_box_change(q1, q2, model, tp, word_similarity, threshold=0.5):
     """
-    'q1' and 'q2' are the two questions which are detected as similar by the classifier: score >= 0.4.
+    'q1' and 'q2' are the two questions which are detected as similar by the classifier: score >= 0.5.
     'model' is the classifier used by the oracle, which in this case returns a similarity score
       between 0 and 1 since it is an open box algorithm.
     The function changes 'q2' so that it retains the same meaning and is now detected as not similar to q2
@@ -8,15 +8,17 @@ def adversarial_white_box_change(q1, q2, model, tp, word_similarity, threshold=0
     q1_tokenized = tp.tokenize(q1)
     q2_tokenized = tp.tokenize(q2)
     successful = False
-    replaced_words = 0
+    replaced_words = []
+    replacing_words = []
     print("initial q1: {}".format(q1))
     print("initial q2: {}".format(q2))
     print("Initial similarity is {}".format(model.predict_single(tp.detokenize(q1_tokenized), tp.detokenize(q2_tokenized))))
     
-    while not successful or replaced_words >= 5:
+    while not successful or len(replaced_words) >= 5:
         # Try changing each word in q2. At the end, select the change that gives the best improvement
         min_score = model.predict_single(tp.detokenize(q1_tokenized), tp.detokenize(q2_tokenized))
         new_q2 = q2_tokenized
+        candidate_replaced_word = None
         
         for i, word in enumerate(q2_tokenized):
             if word_similarity.contains_word(word):
@@ -28,19 +30,22 @@ def adversarial_white_box_change(q1, q2, model, tp, word_similarity, threshold=0
                 if score < min_score:
                     min_score = score
                     new_q2 = q2_modified
+                    candidate_replaced_word = word
+                    candidate_replacing_word = closest_word
                     
         if new_q2 == q2_tokenized:
             break
         
         if min_score < model.predict_single(tp.detokenize(q1_tokenized), tp.detokenize(q2_tokenized)):
             q2_tokenized = new_q2
-            replaced_words += 1
+            replaced_words.append(candidate_replaced_word)
+            replacing_words.append(candidate_replacing_word)
             
         
         print()
         print("q1: '{}'".format(tp.detokenize(q1_tokenized)))
         print("q2: '{}'".format(tp.detokenize(new_q2)))
-        print("Replacing {} words we have a similarity of {}".format(replaced_words, min_score))
+        print("Similarity: {}. Replaced words {} with {}".format(min_score, replaced_words, replacing_words))
         
         if min_score < threshold:
             successful = True
