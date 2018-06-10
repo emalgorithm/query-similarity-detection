1c132f164579dd48ccdf2c357b98376ca45aea18
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-06-10 11:56:44 +0100

    Create class for words in common model

---
+++ adversarial_attacks/words_in_common_model.py
@@ -0,0 +1,13 @@
+import pickle
+
+
+class WordsInCommonModel():
+    def __init__(self):
+        path_prefix = 'models/words_in_common/'
+        self.model = pickle.load(open(path_prefix + 'model.dump', 'rb'))
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
+
+    def predict_single(self, q1, q2):
+        return self.model.predict_single(q1, q2)
